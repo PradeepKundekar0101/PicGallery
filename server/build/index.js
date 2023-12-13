072b3ab5e298037b74b264463a2ef9a669696bd3
Pradeep Kundekar <pradeepkundekar1010@gmail.com>
2023-12-13 12:15:51 +0530

    Added db connection

--- server/build/index.js
+++ server/build/index.js
@@ -1,4 +1,13 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
@@ -7,10 +16,22 @@ const express_1 = __importDefault(require("express"));
 const dotenv_1 = __importDefault(require("dotenv"));
 const cors_1 = __importDefault(require("cors"));
 const post_1 = __importDefault(require("./routes/post"));
+const mongoose_1 = __importDefault(require("mongoose"));
 dotenv_1.default.config();
 const app = (0, express_1.default)();
 app.use((0, cors_1.default)());
 app.use(express_1.default.json());
+const connect = () => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        const MONGOURI = process.env.MONGO_URI;
+        yield mongoose_1.default.connect(MONGOURI);
+        console.log("DB connected");
+    }
+    catch (error) {
+        console.log(error.message);
+    }
+});
+connect();
 const PORT = process.env.PORT || 8080;
 app.get("/", (req, res) => { res.send("Hello from the backend"); });
 app.use("/api/v1/post", post_1.default);
--- server/src/index.ts
+++ server/src/index.ts
@@ -2,12 +2,24 @@ import express from 'express';
 import dotenv from 'dotenv'
 import cors from 'cors';
 import postRoutes from './routes/post'
+import mongoose, { Error } from 'mongoose';
 dotenv.config();
 
 const app = express();
 
 app.use(cors());
 app.use(express.json())
+
+const connect = async()=>{
+    try {
+        const MONGOURI = process.env.MONGO_URI;
+        await mongoose.connect(MONGOURI as string);
+        console.log("DB connected");
+    } catch (error:any) {
+        console.log(error.message)
+    }
+}
+connect();
 
 const PORT = process.env.PORT || 8080
 app.get("/",(req,res)=>{res.send("Hello from the backend")});
